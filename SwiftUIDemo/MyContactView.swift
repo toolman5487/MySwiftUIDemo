bcec74f2648c5d7a0699aac0c384cc125d5d9165
Willy <willy548798@gmail.com>
2024-12-24 16:49:00 +0800

    SwfitUI Demo v2

---
+++ SwiftUIDemo/MyContactView.swift
@@ -0,0 +1,18 @@
+//
+//  MyContactView.swift
+//  SwiftUIDemo
+//
+//  Created by Willy Hsu on 2024/12/24.
+//
+
+import SwiftUI
+
+struct MyContactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyContactView()
+}
